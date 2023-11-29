c6771f6b35ae49fae33a8c7835bd780c50b6cecc
Seddiqui <ab.mateenseddiqui@gmail.com>
2023-11-30 01:26:33 +0430

    delete feature added

--- customer_management/urls.py
+++ customer_management/urls.py
@@ -7,5 +7,6 @@ urlpatterns= [
     path('logout/', views.logout_user, name='logout_user'),
     path('home/', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('show_record/<int:pk>', views.show_record, name='show_record')
+    path('show_record/<int:pk>', views.show_record, name='show_record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
 ]
